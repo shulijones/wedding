14387fef7ac08191b16fac25ac4a5740173e71da
jonsh <jonsh@mit.edu>
2026-06-16 22:19:51 -0700

    Show users their previous RSVPs

--- rsvp.js
+++ rsvp.js
@@ -5,6 +5,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
   var lname = this.elements.lname.value;
   var url = 'https://script.google.com/macros/s/AKfycbzLNuguRzjJq0dX43242RObKG0NkvRuGpe7XIVmMGTZboFY1WwBobd69TFHW1thGoc9/exec?fname=' + fname + '&lname=' + lname;
 
+  console.log('url ', url);
   fetch(url,
     {
       redirect: "follow",
@@ -22,12 +23,12 @@ document.getElementById('form').addEventListener('submit', function (event) {
     })
     .then(json => {
       var idAndNames = json.data;
-      if (idAndNames === "Name not found") {
+      console.log('ids ', idAndNames);
+      if (idAndNames.status === "Name not found") {
         document.getElementById('output').innerHTML = `<div>Couldn't find guest name &mdash; please double-check and try again.</div>`;
-      } else if (idAndNames === "Already RSVPd") {
-        document.getElementById('output').innerHTML = `<div>This guest has already submitted an RSVP.</div>`;
       } else {
-        create_rsvpPage1(idAndNames);
+        console.log('info ', json.info);
+        create_rsvpPage1(idAndNames, json.info);
       }
     })
     .catch(error => {
@@ -35,15 +36,33 @@ document.getElementById('form').addEventListener('submit', function (event) {
     });
 });
 
-function getEventRsvpButtons(name) {
+function getEventRsvpButtons(name, alreadyRsvpdInfo) {
+  const rsvpd = alreadyRsvpdInfo.length > 0;
+  var idx = 3;
+  if (name === 'brewery') {
+    idx = 4;
+  } else if (name === 'brunch') {
+    idx = 5;
+  }
+
   return `<div class="radios">
-  <label>I'll be there! <input type="radio" name="${name}" id="yes-${name}" /></label><br>
-  <label>Not sure yet <input type="radio" name="${name}" id="maybe-${name}" /></label><br>
-  <label>Can't make it <input type="radio" name="${name}" id="no-${name}" /></label><br>
+  <label>I'll be there! <input type="radio" ${(rsvpd && (alreadyRsvpdInfo[0][idx] >= 1)) ? 'checked' : ''} name="${name}" id="yes-${name}" /></label><br>
+  <label>Not sure yet <input type="radio" ${(rsvpd && (alreadyRsvpdInfo[0][idx] > 0) && (alreadyRsvpdInfo[0][idx] < 1)) ? 'checked' : ''} name="${name}" id="maybe-${name}" /></label><br>
+  <label>Can't make it <input type="radio" ${(rsvpd && (alreadyRsvpdInfo[0][idx] == 0)) ? 'checked' : ''} name="${name}" id="no-${name}" /></label><br>
   </div>`
 }
 
-function create_rsvpPage1(idAndNames) {
+function escapeStr(str) {
+  if (!str) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+function create_rsvpPage1(idAndNames, alreadyRsvpdInfo) {
   const data = [];
   const inputArray = idAndNames.split(',');
   const inviteID = inputArray.shift() || "NA";
@@ -60,27 +79,32 @@ function create_rsvpPage1(idAndNames) {
     }
   });
 
+  const rsvpd = alreadyRsvpdInfo.length > 0;
+  const openingMessage = rsvpd ? 
+    "You already RSVPd! Here's what we have from you &mdash; feel free to edit it and resubmit if something has changed." 
+    : "Will you be able to celebrate with us?";
+
   // Write HTML and checkboxes for each person
   document.getElementById('entireForm').innerHTML = `
-      <div id="form-inner-container"><span class="form-text">Will you be able to celebrate with us?</span><br>
+      <div id="form-inner-container"><span class="form-text">${openingMessage}</span><br>
       <img src="./photo/flourish.png" style="width:75px"><br>
       ${data.map(
-    person => `<div class="form-section"><span class="guest-name">${person.name}:</span><br>
+    (person, i) => `<div class="form-section"><span class="guest-name">${person.name}:</span><br>
       <div class="radios">
-      <label>I'll be there! <input type="radio" name="rsvp-${person.id}" id="1-rsvp-${person.id}" /></label><br/>
-      <label>I won't be there <input type="radio" name="rsvp-${person.id}" id="0-rsvp-${person.id}" /></label><br/>
+      <label>I'll be there! <input type="radio" ${rsvpd && alreadyRsvpdInfo[i][1] ? 'checked' : ''} name="rsvp-${person.id}" id="1-rsvp-${person.id}" /></label><br/>
+      <label>I won't be there <input type="radio" ${rsvpd && !alreadyRsvpdInfo[i][1] ? 'checked' : ''} name="rsvp-${person.id}" id="0-rsvp-${person.id}" /></label><br/>
       </div>
-      Any dietary restrictions? <input type="text" id="diet-${person.id}" /></div><br/><br/>`
-    ).join('')}
+      Any dietary restrictions? <input type="text" value="${rsvpd ? escapeStr(alreadyRsvpdInfo[i][2]) : ''}" id="diet-${person.id}" /></div><br/><br/>`
+  ).join('')}
       <img src="./photo/flourish.png" style="width:75px">
       <div class="form-section">Please give us an email address we can use to contact you about the wedding. You can put multiple emails if you'd like &mdash; please separate them with commas.
-      <input type="text" id="email" style="width:70%"/></div><br><br>
+      <input type="text" value="${rsvpd ? escapeStr(alreadyRsvpdInfo[0][6]) : ''}" id="email" style="width:70%"/></div><br><br>
       We know you might not be sure yet, but we'd love a general sense of if you'll be able to make it to the events we're planning.
       (See the Schedule for more details.)<br><br>
-      <div class="form-section"><span class="event-name">Thursday from 6-10 pm: Exploratorium after Dark (18+)</span>${getEventRsvpButtons("explor")}</div><br>
-      <div class="form-section"><span class="event-name">Friday from 2-4 pm: TODO brewery</span>${getEventRsvpButtons("brewery")}</div><br>
-      <div class="form-section"><span class="event-name">Sunday from 10:30 am onwards: Brunch at our house</span>${getEventRsvpButtons("brunch")}</div><br>
-      <div class="form-section">Anything else you'd like to tell us? <input type="text" id="extra" /></div><br>
+      <div class="form-section"><span class="event-name">Thursday from 6-10 pm: Exploratorium after Dark (18+)</span>${getEventRsvpButtons("explor", alreadyRsvpdInfo)}</div><br>
+      <div class="form-section"><span class="event-name">Friday from 2-4 pm: TODO brewery</span>${getEventRsvpButtons("brewery", alreadyRsvpdInfo)}</div><br>
+      <div class="form-section"><span class="event-name">Sunday from 10:30 am onwards: Brunch at our house</span>${getEventRsvpButtons("brunch", alreadyRsvpdInfo)}</div><br>
+      <div class="form-section">Anything else you'd like to tell us? <input type="text" value="${rsvpd ? escapeStr(alreadyRsvpdInfo[0][7]) : ''}" id="extra" /></div><br>
       <button id="submit" type="submit">Submit</button>
       <div id="form-output"></div>
       </div>
@@ -94,8 +118,10 @@ function create_rsvpPage1(idAndNames) {
     });
     eventData = [];
     for (event of ["explor", "brewery", "brunch"]) {
+      const attendingStr = document.querySelector(`input[name="${event}"]:checked`).id.split('-')[0];
+      const attendingVal = attendingStr === 'yes' ? 1 : (attendingStr === 'maybe' ? 0.5 : 0);
       eventData.push({
-        attending: document.querySelector(`input[name="${event}"]:checked`).id.split('-')[0]
+        attending: attendingVal
       })
     }
     var response = {};
@@ -127,7 +153,7 @@ function submitForm(data) {
 
       // Check both status and the specific response string (equivalent to xhr.onload)
       if (response.status === 200 && text === 'Success') {
-        document.getElementById('entireForm').innerHTML = `<div>Success!</div>`;
+        document.getElementById('entireForm').innerHTML = `<div>Thanks for your RSVP!</div>`;
       } else {
         document.getElementById('form-output').innerHTML = `<div>Something went wrong. Check your internet connection and try again. If this persists, you can just send Shuli and Evan your RSVP directly.</div>`;
         document.getElementById('submit').disabled = false;
